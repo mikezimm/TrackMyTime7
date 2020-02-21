28a61454ff375161406c17857515985549c4ca0c
mikezimm <49648086+mikezimm@users.noreply.github.com>
2020-02-20 19:44:48 -0500

    Update DebugStyles.tsx

--- src/webparts/trackMyTime7/components/DebugStyles.tsx
+++ src/webparts/trackMyTime7/components/DebugStyles.tsx
@@ -2,11 +2,52 @@ import * as React from 'react';
 
 import { Pivot, PivotItem, PivotLinkSize, PivotLinkFormat, IPivotStyles, IPivotStyleProps } from 'office-ui-fabric-react/lib/Pivot';
 
+export const myLilac = "#EBD0FF";
+export const myGreen = "#BBFFB0";
+export const myYellow = "#FFFAB0";
+export const myBlue = "#B0DEFF";
+export const myOrange = "#FFDCB0";
+export const myRed = "#FFC1B0";
+
+
 
 export function mainPivot(debugMode, display) {
     let ret = {
         root: {
-            backgroundColor: debugMode ? '#B0DEFF' : 'transparent',
+            backgroundColor: debugMode ? myBlue : 'transparent',
+            borderColor: debugMode ? '#2566CA' : 'transparent',
+            //...(cardSectionOrItemStyles.root as object)
+          }
+    };
+    return ret;
+}
+
+export function projectList(debugMode, display) {
+    let ret = {
+        root: {
+            backgroundColor: debugMode ? myYellow : 'transparent',
+            borderColor: debugMode ? '#2566CA' : 'transparent',
+            //...(cardSectionOrItemStyles.root as object)
+          }
+    };
+    return ret;
+}
+
+export function itemList(debugMode, display) {
+    let ret = {
+        root: {
+            backgroundColor: debugMode ? myGreen : 'transparent',
+            borderColor: debugMode ? '#2566CA' : 'transparent',
+            //...(cardSectionOrItemStyles.root as object)
+          }
+    };
+    return ret;
+}
+
+export function entryChoice(debugMode, display) {
+    let ret = {
+        root: {
+            backgroundColor: debugMode ? myLilac : 'transparent',
             borderColor: debugMode ? '#2566CA' : 'transparent',
             //...(cardSectionOrItemStyles.root as object)
           }
